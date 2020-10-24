f08d4d14d8b0e08952633c16c5dc70db3c55c2d6
wushiai <2625957707@qq.com>
2020-10-24 14:45:08 +0800

    first push

---
+++ src/test/java/com/geek/geekstudio/GeekstudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.geekstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekstudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
